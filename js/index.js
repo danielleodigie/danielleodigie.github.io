9411f660fa7d6bb776f655c035a09505e5108127
danielleodigie <odigiedanielle@gmail.com>
2022-01-13 10:59:09 -0500

    new commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function projectScroll(){
+	var element = document.getElementById("projects");
+	element.scrollIntoView({behavior: "smooth"});
+}
+
+function aboutScroll(){
+	var element = document.getElementById("main");
+	element.scrollIntoView({behavior: "smooth"});
+}
+
+function contactScroll(){
+	var element = document.getElementById("contact");
+	element.scrollIntoView({behavior: "smooth"});
+}
